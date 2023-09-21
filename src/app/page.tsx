3a6f528181ccc1e43d56ee3152e17dd5df56eec0
Kelson Victor <98909425+K3lOli@users.noreply.github.com>
2023-09-21 18:00:43 -0300

    adicionando um elemento de load no carregamento da página

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,18 @@ import { Body_2 } from "./components/feed_2";
 import { Body_3 } from "./components/feed3/index";
 import { DarkModeButton } from "./components/darkModeButton";
 import { useEffect, useState } from "react";
+import { div } from "three/examples/jsm/nodes/Nodes.js";
 
 export default function Home() {
   const [isDarkMode, setIsDarkMode] = useState(false);
   const [screenWidth, setScreenWidth] = useState<number>(0);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
 
   useEffect(() => {
     const updateWindowDimensions = () => {
@@ -17,16 +25,15 @@ export default function Home() {
     };
 
     // Adicionar um ouvinte de redimensionamento quando o componente for montado
-    window.addEventListener('resize', updateWindowDimensions);
+    window.addEventListener("resize", updateWindowDimensions);
 
     // Chamar a função de atualização inicialmente
     updateWindowDimensions();
 
     // Remover o ouvinte de redimensionamento quando o componente for desmontado
     return () => {
-      window.removeEventListener('resize', updateWindowDimensions);
+      window.removeEventListener("resize", updateWindowDimensions);
     };
-
   }, []);
 
   return (
@@ -38,11 +45,16 @@ export default function Home() {
       } overflow-x-hidden`}
     >
       <div>
+        
+        {isLoading ? <div className="flex justify-center items-center h-screen w-screen">
+          <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-violet-900"></div>
+        </div> : <div>
         <DarkModeButton isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
         <Header isDarkMode={isDarkMode} />
-        <Body isDarkMode={isDarkMode} screenWidth={screenWidth}/>
-        <Body_2 isDarkMode={isDarkMode} screenWidth={screenWidth}/>
-        <Body_3 isDarkMode={isDarkMode} screenWidth={screenWidth}/>
+        <Body isDarkMode={isDarkMode} screenWidth={screenWidth} />
+        <Body_2 isDarkMode={isDarkMode} screenWidth={screenWidth} />
+        <Body_3 isDarkMode={isDarkMode} screenWidth={screenWidth} /></div>}
+        
       </div>
     </body>
   );
